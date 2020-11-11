470f63386429edad73cfe87e08d413815e3bfc6f
A.Dabrowska <aledabrowska@gmail.com>
2020-11-11 10:46:50 +0100

    start

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let name = "1";
+let surname = "2";
+let trzecia = "5";
+
+let toto = [name, surname, trzecia]
+
+console.log(name);
+console.log(surname);
+console.log(trzecia);
+
+console.log(toto);
